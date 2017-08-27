7d6552b0b14856326e8a95c5189f8707672f4fc6
Colin Bernard <colinjbernard@hotmail.com>
2017-08-26 19:14:07 -0700

    Changed google map styling, added infowindow functionality

--- map.js
+++ map.js
@@ -4,26 +4,343 @@
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: new google.maps.LatLng(49.9569, -119.3787),
-		zoom: 7
+		zoom: 7,
+		styles: [
+		  {
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#1d2c4d"
+		      }
+		    ]
+		  },
+		  {
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#8ec3b9"
+		      }
+		    ]
+		  },
+		  {
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#1a3646"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "administrative.country",
+		    "elementType": "geometry.stroke",
+		    "stylers": [
+		      {
+		        "color": "#4b6878"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "administrative.land_parcel",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#64779e"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "administrative.province",
+		    "elementType": "geometry.stroke",
+		    "stylers": [
+		      {
+		        "color": "#4b6878"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "landscape.man_made",
+		    "elementType": "geometry.stroke",
+		    "stylers": [
+		      {
+		        "color": "#334e87"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "landscape.natural",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#023e58"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "poi",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#283d6a"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "poi",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#6f9ba5"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "poi",
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#1d2c4d"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "poi.park",
+		    "elementType": "geometry.fill",
+		    "stylers": [
+		      {
+		        "color": "#023e58"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "poi.park",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#3C7680"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road",
+		    "stylers": [
+		      {
+		        "visibility": "off"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#304a7d"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#98a5be"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road",
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#1d2c4d"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road.arterial",
+		    "elementType": "geometry.fill",
+		    "stylers": [
+		      {
+		        "color": "#1d1d1d"
+		      },
+		      {
+		        "visibility": "on"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road.highway",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#2c6675"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road.highway",
+		    "elementType": "geometry.stroke",
+		    "stylers": [
+		      {
+		        "color": "#255763"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road.highway",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#b0d5ce"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "road.highway",
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#023e58"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#98a5be"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit",
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#1d2c4d"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit.line",
+		    "elementType": "geometry.fill",
+		    "stylers": [
+		      {
+		        "color": "#283d6a"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit.station",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#3a4762"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit.station.airport",
+		    "stylers": [
+		      {
+		        "color": "#ffeb3b"
+		      },
+		      {
+		        "saturation": 50
+		      },
+		      {
+		        "visibility": "on"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit.station.airport",
+		    "elementType": "labels.text",
+		    "stylers": [
+		      {
+		        "color": "#7a0ed7"
+		      },
+		      {
+		        "lightness": 15
+		      },
+		      {
+		        "visibility": "on"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "transit.station.airport",
+		    "elementType": "labels.text.stroke",
+		    "stylers": [
+		      {
+		        "color": "#1d1d1d"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "water",
+		    "elementType": "geometry",
+		    "stylers": [
+		      {
+		        "color": "#0e1626"
+		      }
+		    ]
+		  },
+		  {
+		    "featureType": "water",
+		    "elementType": "labels.text.fill",
+		    "stylers": [
+		      {
+		        "color": "#4e6d70"
+		      }
+		    ]
+		  }
+		]
 	});
-
+	
+	// init info window
+ 	var infoWindow = new google.maps.InfoWindow;
+	
+	// custom icon
 	var icon = new google.maps.MarkerImage(
 		"images/airplane-marker.png"
 	);
 
+	// create markers for each plane
 	$.getJSON('map.php', function(data){
 		console.log(data);
 		data['acList'].forEach(function(aircraft){
 
+			// position and label
 			var latlng = {lat: aircraft['Lat'], lng: aircraft['Long']};
 			var label = aircraft['Mdl'];
 
+
+			// create marker
 			var marker = new google.maps.Marker({
 				map: map,
 				position: latlng,
-				label: label,
+				label: {text: label, color:"orange"},
 				icon: icon
 			});
+
+			// info window text
+            infocontent = "<h3>" + label + "</h3>"
+            	+"<p><b>ICAO: </b>" + aircraft['Icao'] + "<br>"
+            	+"<b>Reg: </b>" + aircraft['Reg'] + "<br />"
+            	+"<b>Operator: </b>" + aircraft['Op'] + "<br />"
+            	+"<b>From: </b>" + aircraft['From'] + "<br />"
+            	+"<b>To: </b>" + aircraft['To'] + "<br />"
+            	+"<b>Speed: </b>" + aircraft['Spd'] + " knots<br />"
+            	+"<b>Altitude: </b>" + aircraft['Alt'] + "<br />"
+            	+"<b>Track: </b>" + aircraft['Trak'] + "</p>";
+
+
+            // Code to show info window when clicked
+            marker.addListener('click', function() {
+            	infoWindow.setContent(infocontent);
+            	infoWindow.open(map, marker);
+ 			});
 		});
 
 
